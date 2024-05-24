b14274049d238dd2dbe2e62e82da06b557a93d00
Sergio <sergiomensing@gmail.com>
2024-05-24 14:58:48 +0200

    update analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@/components/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ModeToggle } from "@/components/mode-toggle";
 
